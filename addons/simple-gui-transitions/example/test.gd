caf4a61f907ad4bd99cfd0f075e06a2cf9fb2929
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2023-01-15 22:58:04 -0300

    test: add new test scene of shader based transitions (WIP)

---
+++ addons/simple-gui-transitions/example/test.gd
@@ -0,0 +1,53 @@
+extends Control
+
+
+# Variables
+onready var _button: Button = find_node("Button")
+onready var _button_2: Button = find_node("Button2")
+
+
+# Built-in overrides
+func _ready() -> void:
+	pass
+
+
+# Event handlers
+func _on_Button_pressed() -> void:
+	var _tween := get_tree().create_tween().parallel()
+	var _controls := [_button, _button_2]
+
+	for i in range(_controls.size()):
+		var control: Control = _controls[i]
+		var delay := 0.15 * i
+
+		_tween.set_ease(Tween.EASE_IN_OUT)
+		_tween.set_trans(Tween.TRANS_CUBIC)
+		_tween.tween_method(
+			self, "_set_param_slide",
+			Vector2.ZERO,  Vector2(get_viewport().size.x * 1.5, 0),
+			1.0 + delay, [control]
+		)
+		_tween.parallel()
+
+	yield(_tween, "finished")
+
+	_tween = get_tree().create_tween().parallel()
+
+	for i in range(_controls.size()):
+		var control: Control = _controls[i]
+		var delay := 0.15 * i
+
+		_tween.set_ease(Tween.EASE_IN_OUT)
+		_tween.set_trans(Tween.TRANS_CUBIC)
+		_tween.tween_method(
+			self, "_set_param_slide",
+			Vector2(-get_viewport().size.y * 1.5, 0), Vector2.ZERO,
+			1.0 + delay, [control]
+		)
+		_tween.parallel()
+
+
+
+func _set_param_slide(value: Vector2, control: Control) -> void:
+	print(value)
+	(control.material as ShaderMaterial).set_shader_param("slide", value)
