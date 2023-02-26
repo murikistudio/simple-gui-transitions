5c552203c610b7e7bcaf9c3a2daf7379897fb796
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2023-02-26 15:48:47 -0300

    fix: only add and remove singleton at plugin enable and disable

--- addons/simple-gui-transitions/plugin.gd
+++ addons/simple-gui-transitions/plugin.gd
@@ -6,9 +6,10 @@ const SINGLETON_NAME := "GuiTransitions"
 const SINGLETON_PATH := "res://addons/simple-gui-transitions/singleton.gd"
 
 
-func _enter_tree() -> void:
+func enable_plugin() -> void:
 	add_autoload_singleton(SINGLETON_NAME, SINGLETON_PATH)
 
 
-func _exit_tree() -> void:
+func disable_plugin() -> void:
 	remove_autoload_singleton(SINGLETON_NAME)
+
